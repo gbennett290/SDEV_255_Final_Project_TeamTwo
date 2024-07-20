7c43dd7f86cf34ae9a4b62bbe1405a0495526daa
Gabriel Bennett <gbennett29@ivytech.edu>
2024-07-19 21:09:27 -0400

    set up cookies systems

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Course = require('./models/course');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 // express app
 const app = express();
@@ -28,6 +29,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded( { extended: true}));
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.get('/', (req, res) =>{
@@ -98,7 +100,27 @@ app.get('/signin', (req, res) => {
     res.render('signin', { title: 'Sign In' });
 })
 
+// cookies
+app.get('/set-cookies', (req, res) => {
+    // res.setHeader('set-cookie', 'newUser = true');
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+    res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+    const cookies = req.cookies;
+    console.log(cookies.newUser);
+
+    res.json(cookies);
+
+});
+
 // 404 page
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
+
